40512294c9d567c3e455cc5ab0b9b64987bf389c
LeFede <lefedeok@gmail.com>
2023-07-26 11:54:42 -0300

    reset cart on purchase

--- src/pages/Rating/index.tsx
+++ src/pages/Rating/index.tsx
@@ -1,45 +1,48 @@
-import { useEffect, useState } from "react";
-import styles from "./Rating.module.css";
-import { useLocation } from "react-router-dom";
-import Swal from "sweetalert2";
-import { star } from "@/assets";
-import { useDispatch } from "react-redux";
-import { fetchMenus } from "../../redux";
+import { star } from "@/assets"
+import { fetchMenus, resetCart } from "@/redux"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { useLocation } from "react-router-dom"
+import Swal from "sweetalert2"
+import styles from "./Rating.module.css"
+
 export const Rating = () => {
-  const dispatch = useDispatch();
-  const [order, setOrder] = useState<any | null>(null);
-  const currentUrl = useLocation();
-  const params = new URLSearchParams(currentUrl.search);
-  const id: string | null = params.get("orderId");
+  const dispatch = useDispatch()
+  const [order, setOrder] = useState<any | null>(null)
+  const currentUrl = useLocation()
+  const params = new URLSearchParams(currentUrl.search)
+  const id: string | null = params.get("orderId")
   const handleClick = async () => {
     Swal.fire({
       title: "Bien",
       text: "Gracias por su voto",
       icon: "success",
       confirmButtonText: "Aceptar",
-    });
-    await dispatch<any>(fetchMenus());
-  };
+    })
+    await dispatch<any>(fetchMenus())
+  }
   const fetchOrder = async (id: string | null) => {
     if (!id) {
-      return;
+      return
     }
     const response = await fetch(
       `http://resto-back-production-2867.up.railway.app/order/${id}`
-    );
-    const data = await response.json();
-    return data;
-  };
+    )
+    const data = await response.json()
+    return data
+  }
   useEffect(() => {
-    fetchOrder(id).then((data) => setOrder(data));
-  }, []);
+    dispatch(resetCart)
+    fetchOrder(id).then((data) => setOrder(data))
+  }, [])
+
   const updateDishReviews = async (dishId: string, newReview: number) => {
     try {
-      console.log(dishId);
+      console.log(dishId)
       const indexOfDish = order.item.findIndex((x: any) => {
-        return x.dish._id === dishId;
-      });
-      order.item[indexOfDish].dish.reviews.push(newReview);
+        return x.dish._id === dishId
+      })
+      order.item[indexOfDish].dish.reviews.push(newReview)
       const requestOption = {
         method: "PUT",
         headers: {
@@ -48,23 +51,23 @@ export const Rating = () => {
         body: JSON.stringify({
           reviews: order.item[indexOfDish].dish.reviews,
         }),
-      };
+      }
       const response = await fetch(
         `http://resto-back-production-2867.up.railway.app/dish/${dishId}`,
         requestOption
-      );
+      )
       if (response.ok) {
-        console.log("a");
+        console.log("a")
       } else {
-        console.log("entro");
+        console.log("entro")
       }
     } catch (error) {
-      console.log("entro1");
-      console.log(error);
+      console.log("entro1")
+      console.log(error)
     }
-  };
+  }
   if (order === null) {
-    return <h1>Loading...</h1>;
+    return <h1>Loading...</h1>
   }
   return (
     <section className={styles.container}>
@@ -72,12 +75,12 @@ export const Rating = () => {
       <h6>precio total = {order.totalPrice}</h6>
       <section className={styles.dishes}>
         {order.item.map((x: any) => {
-          const reviews = x.dish.reviews;
+          const reviews = x.dish.reviews
           const sum = reviews.reduce(
             (acc: number, review: number) => acc + review,
             0
-          );
-          const average = Math.round((sum / reviews.length) * 100) / 100;
+          )
+          const average = Math.round((sum / reviews.length) * 100) / 100
 
           return (
             <div className={styles.dish}>
@@ -88,49 +91,49 @@ export const Rating = () => {
               <section>
                 <button
                   onClick={() => {
-                    updateDishReviews(x.dish._id, 1);
-                    handleClick();
+                    updateDishReviews(x.dish._id, 1)
+                    handleClick()
                   }}
                 >
                   <img className={styles.star} src={star} alt="" />
                 </button>
                 <button
                   onClick={() => {
-                    updateDishReviews(x.dish._id, 2);
-                    handleClick();
+                    updateDishReviews(x.dish._id, 2)
+                    handleClick()
                   }}
                 >
                   <img className={styles.star} src={star} alt="" />
                 </button>
                 <button
                   onClick={() => {
-                    updateDishReviews(x.dish._id, 3);
-                    handleClick();
+                    updateDishReviews(x.dish._id, 3)
+                    handleClick()
                   }}
                 >
                   <img className={styles.star} src={star} alt="" />
                 </button>
                 <button
                   onClick={() => {
-                    updateDishReviews(x.dish._id, 4);
-                    handleClick();
+                    updateDishReviews(x.dish._id, 4)
+                    handleClick()
                   }}
                 >
                   <img className={styles.star} src={star} alt="" />
                 </button>
                 <button
                   onClick={() => {
-                    updateDishReviews(x.dish._id, 5);
-                    handleClick();
+                    updateDishReviews(x.dish._id, 5)
+                    handleClick()
                   }}
                 >
                   <img className={styles.star} src={star} alt="" />
                 </button>
               </section>
             </div>
-          );
+          )
         })}
       </section>
     </section>
-  );
-};
+  )
+}
